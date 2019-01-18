10b64a249d6497a9ae6fd659b3e6805b1c741e84
Joel Gessner <joelxxzz@gmail.com>
2019-01-18 14:06:35 +0000

    + Completed comments on all the classes

--- Scheduling Algorithm/Source.cpp
+++ Scheduling Algorithm/Source.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <chrono>
+//#include <chrono>
 
-class Node
+class Node // Node class serving as Nodes for our Stack, Queue and Scheduler
 {
 public:
-	Node(int value, Node* nextptr, Node* prevptr = NULL, int currentpriority = 0)
-	{
-		this->value = value;
-		next = nextptr;
-		prev = prevptr;
-		priority = currentpriority;
-	}
+	Node(int val, Node* nextptr, Node* prevptr = nullptr, int currentpriority = 0) : // Node constuctor that takes four parameters, a value, a previous Node pointer, a Next Node pointer and a priority
+		priority(currentpriority), next(nextptr), prev(prevptr), value(val)
+	{}
+
+	// Getters and setters for all the attributes, value, Next pointer, Previous pointer and priority
 	int getVal(void)
 	{
 		return value;
@@ -44,57 +42,58 @@ public:
 		this->priority = priority;
 	}
 private:
+	//Private attributes of the Node class
 	Node* next;
 	Node* prev;
 	int priority;
 	int value;
 };
-class Stack //Works!
+class Stack // Stack class
 {
 public:
-	Stack(void)
-	{
-		top = 0;
-	}
+	Stack(void) : // Initializer list for our stack constructor
+		top(0)
+	{}
 	~Stack(void)
 	{
-
+		delete top; // Decontructor, that will delete the top pointer when the stack object gets destroyed
 	}
-	void Push(int value)
+	void Push(int value) // Push function, meant for pushing Nodes on top of the stack
 	{
-		Node* tmp = new Node(value, top);
-		top = tmp;
+		Node* tmp = new Node(value, top); // Creating a temporary Node to store the next Node in the stack
+		top = tmp; // Assigning that new node to the top Node pointer
+		delete tmp; // Deleting the temporary Node
 	}
-	Node* NodePop(void)
+	Node* NodePop(void) // NodePop function, it returns the first Node item in the stack
 	{
-		Node* tmp = top;
-		if (top != nullptr)
+		Node* tmp = top; // Assigns a temporary Node pointer to the current top Node Pointer
+		if (top != nullptr) // As long as the top pointer isn't a null pointer
 		{
-			top = top->getNext();
+			top = top->getNext(); // Shift the top pointer to the next one in the stack, since we are deleting the temporary Node we assigned before
 		}
-		return tmp;
+		return tmp; // Returns the temporary Node we want to delete from the stack
 	}
-	int Pop(void)
+	int Pop(void) // Pop function, which returns the value of the returned Node from the NodePop function
 	{
-
-		Node* tmp = NodePop();
-		int ret = 0;
-		try
+		Node* tmp = NodePop(); // Assigning a temporary Node to serve as the Node we are deleting 
+		int ret = 0; // Integer that will hold the value of the Node we are deleting
+		try // Try and catch block where we are checking if the queue is empty or not
 		{
-			if (tmp != nullptr)
+			if (tmp != nullptr) // As long as the temporary Node isn't a null pointer
 			{
-				ret = tmp->getVal();
-				delete tmp;
-				return ret;
+				ret = tmp->getVal(); // Assign the value to the integer we are returning
+				delete tmp; // Deleting the temporary Node
+				return ret; // Returning the value of the Node we deleted
 			}
 			else
 			{
-				throw "Empty Stack";
+				delete tmp; // Deleting the temporary Node
+				throw "Empty Stack"; // Throwing a string to the catch block, that will tell the user that the stack is empty
 			}
 		}
-		catch (const char* m)
+		catch (const char* m) // Catching the thrown exception that happened if the stack is empty
 		{
-			std::cout << m << std::endl;
+			std::cout << m << std::endl; // Displays the string to the user
 		}
 	}
 private:
@@ -182,21 +181,22 @@ private:
 		return tmp; // Return the Node we are deleting
 	}
 };
-class Scheduler : public Queue
+class Scheduler : public Queue // Scheduler class inherited from the Queue class
 {
 private:
-	int counter = 0;
-	int val;
-	Node* NodeDequeue(void)
+	int counter = 0; // Integer counter used to count when the same priority of Nodes get deleted in a row
+	int val; // Integer used to store the highest priority value when looping through the Queue
+	Node* NodeDequeue(void) // A implementation of the abstract function defined in the Queue class, that returns the Node we are deleting
 	{
-		Node *tmp = back;
-		val = 0;
-		if (tmp != nullptr)
+		Node *tmp = back; // Assigning the back Node to a temporary variable
+		val = 0; // Assigning the integer that is used to store the highest priority to 0
+		if (tmp != nullptr) // If the temporary Node pointer isn't a null pointer
 		{
-			tmp = nodeToDelete(tmp, val);
+			tmp = nodeToDelete(tmp, val); // Setting the temporary Node pointer to the Node we are deleting
 
-			if (tmp != nullptr)
+			if (tmp != nullptr) // If the temporary Node pointer isn't a null pointer
 			{
+				// Checking all the possible positions the Node can have, and then making sure that the pointers are right when the Node gets deleted
 				if (tmp->getPrev() != nullptr && tmp->getNext() != nullptr)
 				{
 					tmp->getPrev()->setNext(tmp->getNext());
@@ -216,37 +216,36 @@ private:
 		}
 		else
 		{
-			tmp = back;
-			back = front;
+			back = front; // The back pointer will be equal to the front pointer, if the back pointer is a null pointer
 		}
-		return tmp;
+		return tmp; 
 
 	}
 
-	Node* nodeToDelete(Node* tmp, int val)
+	Node* nodeToDelete(Node* tmp, int val) // A fucntion which loops through the Queue with a pointer and returns the Node that needs to be deleted
 	{
-		while (tmp != nullptr)
+		while (tmp != nullptr) 
 		{
-			if (counter < 2)
+			if (counter < 2) // If the counter used to determine if there has been to many items in a row of the same item is less than 2
 			{
-				if (tmp->getPriority() > val)
+				if (tmp->getPriority() > val) // If the priority value of the current Node pointer is bigger than the highest stored value
 				{
-					val = tmp->getPriority();
+					val = tmp->getPriority(); // The highest stored value gets updated to that new priority value
 				}
 			}
-			else
+			else // If the counter is above 2
 			{
-				val = tmp->getPriority();
+				val = tmp->getPriority(); // The value gets assigned to the first Node's priority in the Queue
 			}
-			if (tmp->getNext() == nullptr) break;
-			tmp = tmp->getNext();
+			if (tmp->getNext() == nullptr) break; // We break the loop when the next item is a null pointer so we can continue to loop through the Queue object
+			tmp = tmp->getNext(); // Setting the temporary Node pointer to the next one in the Queue
 
 		}
 
 
-		while (tmp != nullptr)
+		while (tmp != nullptr) // As long as the temporary pointer isn't a null pointer
 		{
-
+			// checking if the counter is above or lower than 2, and from there we either reset the counter or add one to it. And we are also checking if the priority is equal to the value set by the previous loop
 			if (counter < 2 && tmp->getPriority() == val)
 			{
 				counter++;
@@ -257,45 +256,45 @@ private:
 				counter = 0;
 				return tmp;
 			}
-			tmp = tmp->getPrev();
+			tmp = tmp->getPrev(); // Setting the temporary Node pointer to the previous Node in the Queue
 		}
 	}
 };
 
-int main()
-{
-	/*Queue s;
-	s.Enqueue(20, 1);
-	s.Enqueue(10, 8);
-	s.Enqueue(19, 5);
-	s.Enqueue(9, 3);
-	s.Enqueue(9, 4);
-	std::cout << s.Dequeue() << std::endl;
-	std::cout << s.Dequeue() << std::endl;
-	std::cout << s.Dequeue() << std::endl;
-	std::cout << s.Dequeue() << std::endl;
-	std::cout << s.Dequeue() << std::endl;*/
-
-	Scheduler s;
-	int m = (rand() % 10) + 1;
-	int r = (rand() % 500) + 1;
-	for (int i = 0; i < 500; i++)
-	{
-		s.Enqueue(r, m);
-		r = (rand() % 500) + 1;
-		m = (rand() % 10) + 1;
-	}
-
-	//std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	for (int i = 0; i < 500; i++)
-	{
-		std::cout << s.Dequeue() << std::endl;
-	}
-	//std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-
-	//std::cout << "Time difference in ms = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
-
-
-	getchar();
-	return 0;
-}
+//int main()
+//{
+//	Queue s;
+//	s.Enqueue(20, 1);
+//	s.Enqueue(10, 8);
+//	s.Enqueue(19, 5);
+//	s.Enqueue(9, 3);
+//	s.Enqueue(9, 4);
+//	std::cout << s.Dequeue() << std::endl;
+//	std::cout << s.Dequeue() << std::endl;
+//	std::cout << s.Dequeue() << std::endl;
+//	std::cout << s.Dequeue() << std::endl;
+//	std::cout << s.Dequeue() << std::endl;
+//
+//	Scheduler s;
+//	int m = (rand() % 10) + 1;
+//	int r = (rand() % 500) + 1;
+//	for (int i = 0; i < 500; i++)
+//	{
+//		s.Enqueue(r, m);
+//		r = (rand() % 500) + 1;
+//		m = (rand() % 10) + 1;
+//	}
+//
+//	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+//	for (int i = 0; i < 500; i++)
+//	{
+//		std::cout << s.Dequeue() << std::endl;
+//	}
+//	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+//
+//	std::cout << "Time difference in ms = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+//
+//
+//	getchar();
+//	return 0;
+//}
